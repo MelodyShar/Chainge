755ba101dbe0876321c5e783399152c88a494429
Melody <hello@melodyshar.com>
2015-02-28 15:31:09 -0800

    Revert a30c945..6153edb
    This rolls back to commit a30c945e2e25f8a1841e0c9c5a4869bec08d65f0.

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-# require 'rails/all'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'active_model/railtie'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Blitz
+module Chainge
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -24,5 +19,8 @@ module Blitz
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
